7d8a31e42aca2b5eaf9e4b116783d2055e920ab9
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2018-02-24 16:46:01 +0100

    added command-line args; fixed binary file bug

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
+import Control.Exception as E
+import System.IO
 import System.Directory
--- import Control.Monad
--- import Debug.Trace (trace)
+import System.Exit
+import System.Environment
 import qualified Data.Text as T
-
-type Dir'  = FilePath
-type File' = FilePath
+import qualified Data.List as L
 
 lineCount :: String -> Int
 lineCount = length . filter (== '\n')
@@ -32,11 +32,9 @@ getAllFiles' ignoreDirs paths = do
 
   let files' = concat files
   let dirs'  = concatMap prependPaths $ zip paths dirs
-
   let filteredDirs = filterDirs ignoreDirs dirs'
 
   files'' <- getAllFiles' ignoreDirs filteredDirs
-
   return $ files' ++ files''
   where
     prependPaths :: (FilePath, [FilePath]) -> [FilePath]
@@ -52,18 +50,51 @@ isSuffixedWith :: FilePath -> FilePath -> Bool
 isSuffixedWith suffix cs = T.isSuffixOf (T.pack suffix) (T.pack cs)
 
 getAllFiles :: FilePath -> [FilePath] -> [FilePath] -> IO [FilePath]
-getAllFiles path ignoreDirs ignoreFileEndings =
-  getAllFiles' ignoreDirs' [path ++ "/"] >>= \fs ->
+getAllFiles path ignoreDirs ignoreFileEndings = do
+  fs <- getAllFiles' ignoreDirs' [path ++ "/"]
   return $ filteredFiles ignoreFileEndings fs
   where
     ignoreDirs' = map (\d -> path ++ "/" ++ d ++ "/") ignoreDirs
     filteredFiles igfs fs = filter (\f -> not $ or $ map (`isSuffixedWith` f) igfs) fs
 
 getFileSLOC :: FilePath -> IO Int
-getFileSLOC f = lineCount <$> readFile f
+getFileSLOC f = readLineCount `E.catch` handler
+  where
+    readLineCount = do
+      l <- lineCount <$> readFile f
+      l `seq` return l
+
+    handler :: SomeException -> IO Int
+    handler _ = return 0
+
+stripPrefix :: String -> String -> String
+stripPrefix a b = case T.stripPrefix (T.pack a) (T.pack b) of
+  Just x  -> T.unpack x
+  Nothing -> b
+
+parseOpt :: String -> Maybe String -> Maybe [String]
+parseOpt opt arg = (words . stripPrefix opt) <$> arg
+
+slock :: FilePath -> [FilePath] -> [FilePath] -> IO ()
+slock p igds igfs = do
+  fs    <- getAllFiles p igds igfs
+  slocs <- mapM getFileSLOC fs
+  putStrLn $ "sloc: " ++ show (sum slocs)
 
 main :: IO ()
-main =
-  readFile "test/langs/t.c" >>= print >>= \_ ->
-  getAllFiles "./test/langs" [".stack-work", ".git"] [".o", ".out"] >>= \fs ->
-  mapM getFileSLOC fs >>= print . sum
+main = do
+  args <- getArgs
+
+  let igFileFlag  = "--ignore-files="
+  let igDirFlag   = "--ignore-dirs="
+  let ignoreFiles = parseOpt igFileFlag $ L.find (isPrefixedWith igFileFlag) args
+  let ignoreDirs  = parseOpt igDirFlag $ L.find (isPrefixedWith igDirFlag) args
+
+  case (length args, ignoreDirs, ignoreFiles) of
+    (1, Nothing, Nothing)     -> slock (args !! 0) [] []
+    (2, Just igds, Nothing)   -> slock (args !! 0) igds []
+    (2, Nothing, Just igfs)   -> slock (args !! 0) [] igfs
+    (3, Just igds, Just igfs) -> slock (args !! 0) igds igfs
+    (_, _, _) -> do
+      putStrLn "Usage: sloc PATH [--ignore-files=\"\"] [--ignore-dirs=\"\"]"
+      exitFailure
